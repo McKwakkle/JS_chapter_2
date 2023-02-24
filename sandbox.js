a1db6deefbbf1b4143ccf83afcf5612c3ad51bf9
Kellan Mc Naughton <kellanmcn@gmail.com>
2023-02-24 18:12:23 +0200

    Add Sanbox.js
    Test out basic comments

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,18 @@
-alert('hello, world');
+let age = 32;
+let year = 2023;
+
+// log things to console
+console.log(age, year);
+
+
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+
+// Older way to show variables
+var score = 75;
+console.log(score);
